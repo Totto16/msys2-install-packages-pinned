8bfcf9a02fa2a37aa11138bd653009a0ecf872ec
Totto16 <tobiausgais@gmail.com>
2025-05-14 16:12:36 +0200

    feat: add simple webpack config, to build the dist files

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// webpack.config.js
+const path = require("path")
+
+module.exports = {
+	target: "node",
+	entry: "./index.js",
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	mode: "production",
+}
